d79967061196e45563b8e01eb4838a453e3e62c9
fgnievinski <fgnievinski@gmail.com>
2019-07-23 18:25:25 -0300

    Create ardu.ino

---
+++ docs/ardu.ino
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0; url=https://raw.githubusercontent.com/fgnievinski/mphw/master/code/ardu/ardu.ino">
+        <script type="text/javascript">
+            window.location.href = "https://raw.githubusercontent.com/fgnievinski/mphw/master/code/ardu/ardu.ino"
+        </script>
+        <title>Redirect</title>
+    </head>
+    <body>
+        <a href='https://raw.githubusercontent.com/fgnievinski/mphw/master/code/ardu/ardu.ino'>Redirect</a>
+    </body>
+</html>
