b017fb7aae909c4297489f387c5b0fb7a01cca96
fgnievinski <fgnievinski@gmail.com>
2019-09-12 21:37:25 -0300

    Add files via upload

--- code/matlab/battery/do_batt.m
+++ code/matlab/battery/do_batt.m
@@ -1,1 +1,16 @@
-.
+%% load data
+filename = '190909.BAT';
+fid = fopen(filename, 'rt');
+data = textscan(fid, '%2f %2f %2f %2f %2f %2f\t%f');
+fclose(fid);
+data = cell2mat(data);
+time = data(:,4) + data(:,5)./60 + data(:,6)./3600;
+volt = data(:,end);
+
+%%
+figure
+  plot(time, volt, '.-k')
+  grid on
+  xlabel('Time (h)')
+  ylabel('Voltage (V)')
+  title(sprintf('Min.: %g V', min(volt)))
