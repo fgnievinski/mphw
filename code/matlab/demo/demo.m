3e98ad0e72c5e89425498885dd2a6e5d87ab9674
fgnievinski <fgnievinski@gmail.com>
2020-03-03 01:59:08 -0300

    Add files via upload

--- code/matlab/demo/demo.m
+++ code/matlab/demo/demo.m
@@ -1,14 +1,18 @@
-%lib_dir = 'c:\work\misc\fx\read_nmea\lib\';
-%addpath(genpath(lib_dir))
+% Note: make sure you've previously ran ../do_addpath.m
 
-data_dir = 'c:\work\misc\fx\read_nmea\demo\';
+% define the data file location:
+%data_dir = 'c:\work\misc\fx\read_nmea\demo\';
+data_dir = fileparts(mfilename('fullpath'));
 filename = '190110.LOG';
 filepath = fullfile(data_dir, filename);
 if ~exist(filepath, 'file')
     unzip([filepath '.ZIP'])
 end
 
+% load the data:
 nmea = read_nmea_gsv_gps(filepath);
 nmea = fix_nmea_gsv_angle (nmea);
 
+% disply the data for each satellite:
+% (zoom in to inspect just parts of the data)
 snr_plot4_all(nmea, [0 45])
